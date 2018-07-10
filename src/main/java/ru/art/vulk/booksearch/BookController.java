5015e763c454898706f81b7a397afdce7ac78cad
artbmstu <art.bmstu@gmail.com>
2018-07-10 16:36:35 +0300

    Добавлен scroll для работы с elasticsearch. Данные из elasticsearch записываются сразу в Map.

--- src/main/java/ru/art/vulk/booksearch/BookController.java
+++ src/main/java/ru/art/vulk/booksearch/BookController.java
@@ -1,5 +1,9 @@
 package ru.art.vulk.booksearch;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.art.vulk.booksearch.reader.BookReader;
@@ -10,22 +14,22 @@ import java.util.stream.Collectors;
 
 @RestController
 public class BookController {
-
     @Autowired
-    BookRepository repository;
+    TransportClient client;
+
+//    @Autowired
+//    BookRepository repository;
     Map<String, Long> phrases = new HashMap();
 
-    BookController (){}
-
     @GetMapping("/{book}")
-    public String addStudent(@PathVariable String book) throws Exception{
+    public String addStudent(@PathVariable String book) throws Exception {
         List<String> bookRows = BookReader.readBook(book);
         try (FileWriter file = new FileWriter(book + ".json")) {
             int count = 0;
             for (String rowText :
                     bookRows) {
                 count++;
-                file.append("{\"create\":{\"_index\":\"books\",\"_type\" : \""+ book +"\", \"_id\" : \"" + count + "\"}}\n");
+                file.append("{\"create\":{\"_index\":\"books\",\"_type\" : \"" + book + "\", \"_id\" : \"" + count + "\"}}\n");
                 file.append("{\"row\" : \"" + count + "\", \"text\" : \"" + rowText + "\"}\n");
             }
         }
@@ -33,18 +37,19 @@ public class BookController {
     }
 
     @GetMapping("/{book}/topPhrases/{n}")
-    public String topPhrases(@PathVariable String book, @PathVariable int n){
+    public String topPhrases(@PathVariable String book, @PathVariable int n) {
         List<String> words = new ArrayList<>();
-        StringBuffer word = new StringBuffer();
-        Iterator<BookEntity> iterator = repository.findAll().iterator();
-        Map<String, Long> result = new HashMap();
-        int count = 0;
-        String text;
-        Map sorted = null;
-        while (iterator.hasNext()) {
-            count++;
-            if ((text = iterator.next().getText()) != null) {
-                text = text.toLowerCase().replaceAll("[^a-zA-Zа-яА-Я ]", "");
+        StringBuilder word = new StringBuilder();
+        StringBuilder phrase = new StringBuilder();
+
+        SearchResponse scrollResp = client.prepareSearch("books")
+                .setFetchSource("text","row" )
+                .setScroll(new TimeValue(6000))
+                .setSize(10000).get();
+        do {
+            for (SearchHit hit : scrollResp.getHits().getHits()) {
+                String hitText = hit.getSource().values().toString();
+                String text = hitText.toLowerCase().replaceAll("[^a-zA-Zа-яА-Я ]", "");
                 for (int i = 0; i < text.length(); i++) {
                     if (text.charAt(i) != ' ') {
                         word.append(text.charAt(i));
@@ -54,26 +59,27 @@ public class BookController {
                     }
                 }
                 words.add(word.toString());
-            }
-            words.removeIf(e -> e.equals(""));
-            if (n <= words.size()) {
-                StringBuilder s = new StringBuilder();
-                for (int i = 0; i < words.size() - n + 1; i++) {
-                    for (int j = i; j < n + i; j++) {
-                        s.append(words.get(j) + " ");
+                words.removeIf(e -> e.equals(""));
+                if (n <= words.size()) {
+                    for (int i = 0; i < words.size() - n + 1; i++) {
+                        for (int j = i; j < n + i; j++) {
+                            phrase.append(words.get(j) + " ");
+                        }
+                        String string = phrase.toString();
+                        if (phrases.containsKey(string)) {
+                            phrases.put(string, (phrases.get(string) + 1));
+                        } else phrases.put(string, 1L);
+                        phrase.setLength(0);
                     }
-                    String string = s.toString();
-                    if (phrases.containsKey(string)){
-                        phrases.put(string, (phrases.get(string) + 1));
-                    } else phrases.put(string, 1L);
-                    s.setLength(0);
                 }
+                words.clear();
             }
-            words.clear();
-        }
-        result = phrases.entrySet().stream()
+            scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(60000)).execute().actionGet();
+        } while (scrollResp.getHits().getHits().length != 0);
+        Map<String, Long> result = phrases.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        phrases.clear();
         return result.toString();
     }
 }
--- src/main/java/ru/art/vulk/booksearch/BookRepository.java
+++ src/main/java/ru/art/vulk/booksearch/BookRepository.java
@@ -1,8 +1,9 @@
-package ru.art.vulk.booksearch;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends ElasticsearchRepository<BookEntity, Integer> {
-}
+//package ru.art.vulk.booksearch;
+//
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface BookRepository extends ElasticsearchRepository<BookEntity, Integer> {
+//
+//}
